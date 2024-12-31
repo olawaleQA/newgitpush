4a9ba3fb05295885bd39b5f4993489515d58c035
OlawaleQA <Olawaleolowo19@gmail.com>
2024-12-31 14:00:36 +0100

    first commit

---
+++ objects.js
@@ -0,0 +1,10 @@
+const myHouse = {
+    location: "lekki",
+    structure:"Mansion", 
+    colour: "Peach", 
+    materials: "cement", 
+    roofing: "Pop", 
+    worth: 20000000
+
+}
+console.log(myHouse.roofing)
---
+++ slice.js
@@ -0,0 +1,9 @@
+const months = ["Jan", "Feb", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+// months.slice(2,6)
+
+
+months.splice(0,3)
+
+// console.log(months, months.splice(4,4 , 'my custom month'))
+
+console.log(months)
